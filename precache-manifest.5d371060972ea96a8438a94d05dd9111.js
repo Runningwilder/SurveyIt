4abd032f02b68813e137711235ba7c07b0aa17f5
Runningwilder <languageblog96@gmail.com>
2020-11-14 01:26:29 +0200

    first

---
+++ precache-manifest.5d371060972ea96a8438a94d05dd9111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f2ccf7f12a12c0472a8cd85e0c981c",
+    "url": "/SurveyIt/index.html"
+  },
+  {
+    "revision": "9594c72b7411d584def8",
+    "url": "/SurveyIt/static/css/main.2ed9c568.chunk.css"
+  },
+  {
+    "revision": "579162c443db647cbf44",
+    "url": "/SurveyIt/static/js/2.67120b51.chunk.js"
+  },
+  {
+    "revision": "ba747f7848ad46d7397281a03e1191df",
+    "url": "/SurveyIt/static/js/2.67120b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9594c72b7411d584def8",
+    "url": "/SurveyIt/static/js/main.07d9f136.chunk.js"
+  },
+  {
+    "revision": "e481bea9c1a132f15185",
+    "url": "/SurveyIt/static/js/runtime-main.cc7d0ad6.js"
+  }
+]);
