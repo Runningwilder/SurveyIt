b4c2e8b6b874b32749f9cec49f45d8e63427f12a
Runningwilder <languageblog96@gmail.com>
2020-12-07 15:09:54 +0200

    Updates

--- precache-manifest.98d5f411d72ee775a88ccbdcf8fea4ba.js
+++ precache-manifest.98d5f411d72ee775a88ccbdcf8fea4ba.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b6231705bdcac030152f2168e04a982d",
+    "revision": "d6f5afdfb3dc42d3437acea5ab7743b9",
     "url": "/SurveyIt/index.html"
   },
   {
-    "revision": "99d2c83903de8c9c0bec",
+    "revision": "5cb18220c556a13b3cd8",
     "url": "/SurveyIt/static/css/main.9945c763.chunk.css"
   },
   {
-    "revision": "7c22748eb768207e6c7c",
-    "url": "/SurveyIt/static/js/2.d8e3de59.chunk.js"
+    "revision": "942af498d9d53b6201d4",
+    "url": "/SurveyIt/static/js/2.24dd2a61.chunk.js"
   },
   {
     "revision": "d14f21e42c09509e44bd25febcf4b2dc",
-    "url": "/SurveyIt/static/js/2.d8e3de59.chunk.js.LICENSE.txt"
+    "url": "/SurveyIt/static/js/2.24dd2a61.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "99d2c83903de8c9c0bec",
-    "url": "/SurveyIt/static/js/main.0b943228.chunk.js"
+    "revision": "5cb18220c556a13b3cd8",
+    "url": "/SurveyIt/static/js/main.b16b263e.chunk.js"
   },
   {
     "revision": "ba92baf31615c376a105",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SurveyIt/precache-manifest.d902114df82e8200d6f8a1b7549ffcb2.js"
+  "/SurveyIt/precache-manifest.98d5f411d72ee775a88ccbdcf8fea4ba.js"
 );
 
 self.addEventListener('message', (event) => {
